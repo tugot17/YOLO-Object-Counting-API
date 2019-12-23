9784e876c18636a27ca38ea08cc8f26ddd6f8192
tugot17 <piotr635@gmail.com>
2019-12-23 16:55:38 +0100

    remove output.avi

--- count_people_on_image.py
+++ count_people_on_image.py
@@ -3,7 +3,7 @@ import cv2
 from object_counting_api import ObjectCountingAPI
 
 options = {"model": "cfg/yolov2.cfg", "load": "bin/yolov2.weights", "threshold": 0.5, "gpu": 1.0}
-IMG_PATH = "sample_inputs/sample_dog.jpg"
+IMG_PATH = "inputs/united_nations.jpg"
 
 img = cv2.imread(IMG_PATH)
 counter = ObjectCountingAPI(options)
